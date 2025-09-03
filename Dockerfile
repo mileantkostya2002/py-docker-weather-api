474ce7e0fef50bafa121712b52409769b3e9328c
Kostya Mileant <kostya.milya@gmail.com>
2025-09-03 10:52:12 +0200

    solution

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.12
+LABEL maintainer='kostya.m.2002@gmail.com'
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR  /app
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
+
--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
+import requests
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv('API_KEY')
+    FILTER = 'Paris'
+    print(f"API key: {api_key}")
+    if not api_key:
+        print('API key not found!')
+    else:
+        url = 'http://api.weatherapi.com/v1/current.json'
+        result = requests.get(url + f"?key={api_key}&" + f"q={FILTER}")
+
+        if result.status_code == 200:
+            data = result.json()
+            city = data["location"]["name"]
+            country = data["location"]["country"]
+            day_time = data["location"]["localtime"]
+            temperatura = data["current"]["temp_c"]
+            type_weather = data["current"]["condition"]["text"]
+            print(f"{city}/{country} {day_time} "
+                  f"Weather: {temperatura} {type_weather}")
+        else:
+            print("Error: invalid request")
 
 
 if __name__ == "__main__":
